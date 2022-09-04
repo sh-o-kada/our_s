a0202597bbda638ac1a282ca05e2f543e7f82677
sh-o-kada <shokada.25y@gmail.com>
2022-09-05 08:26:49 +0900

    scheduleモデル・バリデーション設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,10 @@ class Schedule < ApplicationRecord
   belongs_to :work_time_1
   belongs_to :work_time_2
   belongs_to :work_time_3
+
+  validates :name,           presence: true
+  validates :work_time_1_id, presence: true
+  validates :start_time,     presence: true
+
+  validates :work_time_1_id, numericality: { other_than: 1, message: "勤務可能な時間帯を選択してください"}
 end
