e39946a2a65a1528500c3af3793f93699bb6218f
sh-o-kada <shokada.25y@gmail.com>
2022-09-05 08:19:54 +0900

    work_time1-3モデル修正

--- app/models/work_time_3.rb
+++ app/models/work_time_3.rb
@@ -1,9 +1,10 @@
 class WorkTime3 < ActiveHash::Base
   self.data = [
-    { id: 1, name: "早番" },
-    { id: 2, name: "日勤" },
-    { id: 3, name: "遅番" },
-    { id: 4, name: "終日" },
+    { id: 1, name: "---" },
+    { id: 2, name: "早番" },
+    { id: 3, name: "日勤" },
+    { id: 4, name: "遅番" },
+    { id: 5, name: "終日" },
   ]
 
   include ActiveHash::Associations
