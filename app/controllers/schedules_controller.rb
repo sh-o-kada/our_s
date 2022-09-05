24838ad16c13426458558be9ebb50624278a5c30
sh-o-kada <shokada.25y@gmail.com>
2022-09-05 13:24:09 +0900

    destroyアクション設定

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -11,6 +11,11 @@ class SchedulesController < ApplicationController
     Schedule.create(schedule_params)
   end
 
+  def destroy
+    schedule = Schedule.find(params[:id])
+    schedule.destroy
+  end
+
   private
   def schedule_params
     params.require(:schedule).permit(:name, :work_time_1_id, :work_time_2_id, :work_time_3_id, :start_time)
